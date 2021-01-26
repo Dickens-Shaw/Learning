395409b54d4247490937780a5a118686fbde6726
肖笛根 <shawdickens@gmail.com>
2021-01-26 14:57:59 +0800

    ✨feat: tsx第3次尝试

---
+++ Qiankun/qiankun-vue/README.md
@@ -0,0 +1,30 @@
+# vue3-element-next
+
+## Project setup
+- 此项目使用脚手架 `npm install -g @vue/cli` or `yarn global add @vue/cli` 的 `vue create <project_name>`命令创建
+- 此项目作以下列配置为基础:
+```bash
+    "element-plus": "^1.0.1-beta.0",
+    "vue": "^3.0.0",
+    "vue-class-component": "^8.0.0-0",
+    "vue-router": "^4.0.0-0",
+    "vuex": "^4.0.0-0"
+
+    "typescript": "~3.9.3"
+```
+支持以`tsx`为 `vue` 默认导出文件类型, 搭载最新的`element-plus` UI 框架
+
+## 使用
+```bash
+    git clone https://github.com/leslie1943/vue3-element-plus-base-cli.git
+```
+
+```bash
+    npm install
+```
+
+```bash
+    npm run dev
+```
+
+开始你的 `Vue3.0` 和 最新的`Element-UI`的开发之旅吧🙃
---
+++ Qiankun/qiankun-vue/src/router/index.ts
@@ -0,0 +1,33 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import Home from '../views/Home'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About')
+  },
+  {
+    path: '/todo',
+    name: 'Todo',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Todo')
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
