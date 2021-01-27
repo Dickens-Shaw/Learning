455a218d45f1caa5d46c5a1bb4cb43b46ba46f4e
肖笛根 <shawdickens@gmail.com>
2021-01-27 17:25:36 +0800

    ✨feat: router

--- Qiankun/qiankun-vue/src/router/index.ts
+++ Qiankun/qiankun-vue/src/router/index.ts
@@ -11,27 +11,18 @@ const routes: Array<RouteRecordRaw> = [
       {
         path: '/home',
         name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/Home')
+        component: () => import(/* webpackChunkName: "home" */ '../views/Home')
       },
       {
         path: '/list',
         name: 'List',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/List')
+        component: () => import(/* webpackChunkName: "list" */ '../views/List')
       },
       {
         path: '/detail',
         name: 'Detail',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () =>
-          import(/* webpackChunkName: "about" */ '../views/Detail')
+          import(/* webpackChunkName: "detail" */ '../views/Detail')
       }
     ]
   }
