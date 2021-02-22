c944b0ae1fc1df4cdfcca82f09deba95b756ff8c
肖笛根 <shawdickens@gmail.com>
2021-02-22 10:10:57 +0800

    daily4

---
+++ JS/daily/day4.js
@@ -0,0 +1,13 @@
+console.log(+true);
+console.log(!"Lydia");
+
+/* 
+  A: 1 and false
+  B: false and NaN
+  C: false and false
+*/
+
+/* 
+  Q:
+  一元操作符加号尝试将 bool 转为 number。true 转换为 number 的话为 1，false 为 0
+*/
